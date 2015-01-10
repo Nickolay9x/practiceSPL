47aca9f1deb8573af7c1dd23c5e80b6a72235ea6
Nikolai <smirnovsg15@gmail.com>
2015-01-10 19:44:44 +0300

    Let's start translater

---
+++ vmachine/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef H_HEADER
+#define H_HEADER
+
+typedef		unsigned char	BYTE;
+typedef		short int		WORD;
+typedef		int				DWORD;
+
+
+
+#endif
---
+++ vmachine/main.c
@@ -0,0 +1,10 @@
+#include "header.h"
+
+int main(void) {
+
+	printf("HELLO\n");
+
+	return 0;
+
+}
+
